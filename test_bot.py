8b82bd5c1ec45e3bf2676d55c8daffa8a5562a7e
WilsonHChung <33712376+WilsonHChung@users.noreply.github.com>
2019-01-19 02:19:56 -0800

    Added help command to identify bot command usage and functionality and added non-functioning role assignment

--- test_bot.py
+++ test_bot.py
@@ -1,14 +1,28 @@
 import discord
 import random
 import config
+from discord.ext import commands
 
 client = discord.Client()
+
+# assigns the bot to a role 
+@client.event
+async def member_join(member):
+    role = discord.utils.get(member.server.roles, name='FRC')
+    await client.add_roles(member, role)
 
 @client.event
 async def on_message(message):
     # we do not want the bot to reply to itself
     if message.author == client.user:
         return
+
+    # displays information about how to use the text 
+    if message.content.startswith('!help'):
+        await client.send_message(message.channel, 'FRC Discord Commands')
+        help_msg = "watch - displays a robotics team's stats including overall ranking, W/L ratio, and match " + "unwatch - stops displaying stats"
+        await client.send_message(message.channel, help_msg)
+    
 
     # prints out basic team information
     if message.content.startswith('!watch '):
@@ -18,6 +32,8 @@ async def on_message(message):
         team_number = int(number[1])
         watch_msg = "Now watching Team " + str(team_number)
         await client.send_message(message.channel, watch_msg)
+
+    # creates a new role to categorize !watch command users     
 
 
 @client.event
